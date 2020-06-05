6fc801febb72bddced1a8e07348ffb8af3a77e7a
rizvi0708abid <abid72rizvi@gmail.com>
2020-06-05 11:35:36 +0530

    Mongo-Kafka-demo-spring-boot-example

--- src/main/java/com/itc/kafkamongo/model/User.java
+++ src/main/java/com/itc/kafkamongo/model/User.java
@@ -1,8 +1,16 @@
 package com.itc.kafkamongo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "User")
 public class User {
     @Id
@@ -10,56 +18,4 @@ public class User {
     private String name;
     private String department;
     private int age;
-
-    public User() {
-    }
-
-    public User(int id, String name, String department, int age) {
-        this.id = id;
-        this.name = name;
-        this.department = department;
-        this.age = age;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", department='" + department + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
